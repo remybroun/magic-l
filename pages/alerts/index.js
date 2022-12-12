081cbbeef4c56907384a5065b221156fff33a824
Remy Broun <remybroun@yahoo.co.uk>
2022-12-11 22:58:42 -0600

    buttonfix

--- pages/alerts/index.js
+++ pages/alerts/index.js
@@ -132,12 +132,12 @@ export default function Alerts(props) {
 
             
             <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-              <div
+              <button
                 onClick={()=>setModalOpen(!modalOpen)}
                 className="inline-flex items-center justify-center rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 sm:w-auto"
               >
                 Export
-              </div>
+              </button>
             </div>
           </div>
 
